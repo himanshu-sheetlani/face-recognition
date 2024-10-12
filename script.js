8108ce202a8ebe7fb68decbcdee5102de53ca204
Himanshu Sheetlani <143863933+himanshu-sheetlani@users.noreply.github.com>
2024-10-12 15:22:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  // faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.faceExpressionNet.loadFromUri('/models')
 ]).then(startVideo)
 
 function startVideo() {
